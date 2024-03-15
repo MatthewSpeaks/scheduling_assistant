4892cb887a95a93ce98e4b6e7207609b47eef76b
Matt Speaks <mattspeaks@hotmail.com>
2024-03-15 06:49:22 -0500

    Initial commit setup candidate and hiring manager model classes

--- src/main/java/ms/schedulingassistant/scheduling_assistant/controllers/UserController.java
+++ src/main/java/ms/schedulingassistant/scheduling_assistant/controllers/UserController.java
@@ -1,17 +1,10 @@
 package ms.schedulingassistant.scheduling_assistant.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("home")
-public class HomeController {
-
-//    @GetMapping
-//    public String displayHome(){
-//
-//    }
-
+@RequestMapping("create_account")
+public class UserController {
 
 }
